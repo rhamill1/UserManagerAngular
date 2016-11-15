baff679187245246cc3ef6a51d42d9d327d51ac1
Arun Sood <arsood@gmail.com>
2016-11-15 15:00:38 -0800

    Angular setup

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-var app = angular.module("userApp", []);
+var app = angular.module("userApp", ["ngRoute"]);
+
+//Configure routes
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/users", {
+            templateUrl: "templates/user-list.html",
+            controller: "UsersController",
+            controllerAs: "usersCtrl"
+        })
+        .otherwise({
+            redirectTo: "/users"
+        });
+});
 
 app.controller("UsersController", function() {
-    
+
 });

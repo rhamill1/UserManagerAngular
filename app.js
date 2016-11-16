48c1209c1f7a11a34bcd22dad1031070b9e2f208
Arun Sood <arsood@gmail.com>
2016-11-15 16:02:23 -0800

    GET and POST for users on homepage

--- app.js
+++ app.js
@@ -13,6 +13,37 @@ app.config(function($routeProvider) {
         });
 });
 
-app.controller("UsersController", function() {
+app.controller("UsersController", function($http) {
+    //Step 1: Make HTTP request to Rails API to retrieve list of users (Hint: look up the $http service)
+    //Step 2: Use Angular's template syntax to display the users
 
+    var vm = this;
+
+    $http({
+        method: "GET",
+        url: "http://localhost:3000/users"
+    }).success(function(users) {
+        vm.users = users;
+    }).error(function() {
+        alert("Error getting users!");
+    });
+
+    vm.submitUser = function(event) {
+        event.preventDefault();
+
+        $http({
+            method: "POST",
+            url: "http://localhost:3000/users",
+            data: {
+                user: vm.user
+            }
+        }).success(function(newUser) {
+            vm.users.push(newUser);
+
+            vm.user = {};
+            $("#add-user-modal").modal("hide");
+        }).error(function() {
+            alert("Error saving user");
+        });
+    }
 });
